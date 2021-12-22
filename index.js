697e03d3f3242a674055bb1671c2189b5a84d090
Will Lovell <willard.lovell@icloud.com>
2021-12-22 13:41:03 -0700

    test commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+
+
+
+const userData = {
+      name: "Steve",
+      email: "steve@steve.com",
+   
+   };
+
+function submitData() {
+   fetch("http://localhost:3000/users",  {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+         Accept: "application/json",
+      },
+   body: JSON.stringify(userData),
+      })
+   .then(respond => respond.json())
+   .then((json) => postData(json))
+   .catch(function (error) {
+      alert("Unauthorized Access")
+     })
+}
+
+function postData() {
+   const main = document.querySelector('main');
+   userData.forEach(user => {
+   const h2 = document.createElement('h2');
+     h2.innerHTML = user.name;
+     h2.innerHTML = user.email;
+     main.appendChild(h2);
+     
+   });
+
