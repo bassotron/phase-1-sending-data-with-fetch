68c145c36c0854a84c3664ef99131fb73d9be8e1
Will Lovell <willard.lovell@icloud.com>
2021-12-22 14:11:37 -0700

    commit tree 42d47c9774f7fb1a69363e230f46e78b1ceaa0bc

--- index.js
+++ index.js
@@ -1,35 +1,22 @@
 
 
 
-const userData = {
-      name: "Steve",
-      email: "steve@steve.com",
-   
-   };
 
-function submitData() {
+function submitData(name, email) {
    fetch("http://localhost:3000/users",  {
       method: "POST",
       headers: {
          "Content-Type": "application/json",
          Accept: "application/json",
       },
-   body: JSON.stringify(userData),
+   body: JSON.stringify( {
+      name, email
+   }),
       })
    .then(respond => respond.json())
-   .then((json) => postData(json))
-   .catch(function (error) {
-      alert("Unauthorized Access")
-     })
+   .then((userData) => document.body.innerHTML = userData.id)
+   .catch((error) => { alert("Unauthorized Access");
+   console.log(error.message)})
 }
 
-function postData() {
-   const main = document.querySelector('main');
-   userData.forEach(user => {
-   const h2 = document.createElement('h2');
-     h2.innerHTML = user.name;
-     h2.innerHTML = user.email;
-     main.appendChild(h2);
-     
-   });
-
+// document.body.innerHTML = error.message
